b2028f4813420bcdacd17f162be42a08e700d336
mithilesh kumar singh <mithileshdotnet@gmail.com>
2022-04-26 23:30:10 +0530

    Refactored the codes

--- CV_AquaERP_repo/AquaERP.API/AquaERP.TestProject/HrDepartmentMasterControllerTest.cs
+++ CV_AquaERP_repo/AquaERP.API/AquaERP.TestProject/HrDepartmentMasterControllerTest.cs
@@ -40,7 +40,6 @@ namespace AquaERP.TestProject
         [Fact]
         public void AddDepartmentTest()
         {
-            //OK RESULT TEST START
 
             //Arrange
             DepartmentMaster addDepartment = new DepartmentMaster()
